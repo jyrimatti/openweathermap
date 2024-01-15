e4c4249db14be9bd2266b327cfd03c83eb577fc9
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-01-15 22:04:59 +0200

    update nix

--- cmd/current_brightness.sh
+++ cmd/current_brightness.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure -i dash -I channel:nixos-23.05-small -p dash curl cacert jq bc
+#! nix-shell --pure -i dash -I channel:nixos-23.11-small -p dash curl cacert jq bc
 set -eu
 
 lat=$(cat .openweathermap-lat)
