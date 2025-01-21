50ce169e5fd14d755a5fdaa869aafd5250479ece
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2025-01-21 20:36:19 +0200

    update nix

--- curl_cached.sh
+++ curl_cached.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure --keep XDG_RUNTIME_DIR --keep CURL_CACHED_MINUTES -i dash -I channel:nixos-23.11-small -p dash cacert curl flock
+#! nix-shell --pure --keep XDG_RUNTIME_DIR --keep CURL_CACHED_MINUTES -i dash -I channel:nixos-24.11-small -p dash cacert curl flock
 set -eu
 
 url="$1"
--- openweathermap_get.sh
+++ openweathermap_get.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --pure --keep CREDENTIALS_DIRECTORY --keep XDG_RUNTIME_DIR -i dash -I channel:nixos-23.11-small -p nix dash curl cacert flock
+#! nix-shell --pure --keep CREDENTIALS_DIRECTORY --keep XDG_RUNTIME_DIR -i dash -I channel:nixos-24.11-small -p nix dash curl cacert flock
 set -eu
 
 . ./openweathermap_env.sh
